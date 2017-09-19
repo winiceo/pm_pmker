6bd0a237e9cd5433845ec87dfbc3e64d58fb5fab
赵少辉 <zhaoshaohui@pmkoo.com>
2017-09-19 16:44:15 +0800

    add pm2.json

---
+++ deploy/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  prodPath: '/pmker/',
+  prodHost: [
+    {
+      host: '47.94.99.63',
+      username: 'root',
+      password: 'Zudaoye123!!@#%*!#@!',
+      agent: process.env.SSH_AUTH_SOCK
+    }
+  ]
+};
